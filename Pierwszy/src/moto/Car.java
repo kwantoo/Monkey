854b9849d2f3dbae65419d98bd1df595d0ba6e5f
Krzysztof Wantuch <wantuch.krzysztof@gmail.com>
2016-10-19 20:01:18 +0200

    kolejna zmiana

---
+++ Pierwszy/src/moto/Car.java
@@ -0,0 +1,29 @@
+package moto;
+
+import java.util.Scanner;
+
+
+public class Car {
+	public int prodYear;
+	public int ccm;
+	public double kWatts;
+	public String brand;
+	public double mnoznik = 1.36;
+	public double horsePwr;
+	
+	 Scanner odczyt = new Scanner(System.in);{
+		System.out.println("Podaje markę samochodu: ");
+		brand = odczyt.next();
+		System.out.println("Podaje rok produkcji: ");
+	    prodYear = odczyt.nextInt();
+	    System.out.println("Podaje pojemność: ");
+	    ccm = odczyt.nextInt();
+	    System.out.println("Podaje moc w kW: ");
+	    kWatts = odczyt.nextDouble();
+	    odczyt.close();
+	    horsePwr = kWatts * mnoznik;
+	    
+	    
+	System.out.println(brand + " " + prodYear + " " + ccm + " " + kWatts + " " + horsePwr);
+	
+}}
---
+++ Pierwszy/src/moto/Inspection.java
@@ -0,0 +1,12 @@
+package moto;
+
+
+public class Inspection {
+
+	
+	public static void main(String[] args) {
+		Car pojazd = new Car();
+		
+	}
+
+}
