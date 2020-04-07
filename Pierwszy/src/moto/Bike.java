205cdcd12e9a28d35f2e3270148beea37eb8f6ae
Kwantoo <kih0n1@gmail.com>
2020-04-07 23:14:21 +0200

    Trzeci commit

--- Pierwszy/src/moto/Bike.java
+++ Pierwszy/src/moto/Bike.java
@@ -2,4 +2,12 @@ package moto;
 
 public class Bike {
 
+	public int yearOfProduction;
+	public double wheelSize;
+	public String modelName;
+	public String bikeType;
+	public String brandName;
+	
+	
+	
 }
